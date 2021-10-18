f72de41ee3567fa625ed6a19b173bc10d3fb1463
thaqnos <thaisaquino1994@gmai.com>
2021-10-18 08:42:08 -0300

    Cria classes e aplica heranças

---
+++ src/application/Program.java
@@ -0,0 +1,16 @@
+package application;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		/*MOTIVAÇÃO:
+		 * Suponha um negócio de banco que possui uma conta comum e uma conta para
+		 * empresas, sendo que a conta para a empresa possui todos os membros da conta comum,
+		 * mais um limite de empréstimo e uma operação de realizar emprestimo. */
+		
+		
+
+	}
+
+}
