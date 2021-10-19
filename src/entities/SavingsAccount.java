1677919271f6f136be1db7d9b598410782096b24
thaqnos <thaisaquino1994@gmai.com>
2021-10-19 08:20:28 -0300

    Adiciona notação final em subclasse SavingsAccount

--- src/entities/SavingsAccount.java
+++ src/entities/SavingsAccount.java
@@ -1,6 +1,6 @@
 package entities;
 
-public class SavingsAccount extends Account {
+public final class SavingsAccount extends Account {
 	
 	private Double interestRate;
 	
@@ -27,7 +27,7 @@ public class SavingsAccount extends Account {
 	}
 	
 	@Override
-	public void withdraw(Double amount) {
+	public final void withdraw(Double amount) {
 		balance -= amount;
 	}
 
