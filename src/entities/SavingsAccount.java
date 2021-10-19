712456c95435452464d5ca86cac39d947e4ce5f8
thaqnos <thaisaquino1994@gmai.com>
2021-10-19 08:06:42 -0300

    Aplica conceitos @Override

--- src/entities/SavingsAccount.java
+++ src/entities/SavingsAccount.java
@@ -25,5 +25,10 @@ public class SavingsAccount extends Account {
 	public void updateBalance() {
 		balance += balance * interestRate;
 	}
+	
+	@Override
+	public void withdraw(Double amount) {
+		balance -= amount;
+	}
 
 }
