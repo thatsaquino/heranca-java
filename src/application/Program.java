d5a48edc77f538d0bfc62868f4f649339e4f2d52
thaqnos <thaisaquino1994@gmai.com>
2021-10-19 07:58:18 -0300

    Adiciona exemplos de downcasting e upcasting

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,16 +1,45 @@
 package application;
 
+import entities.Account;
+import entities.BusinessAccount;
+import entities.SavingsAccount;
+
 public class Program {
 
 	public static void main(String[] args) {
 		
-		/*MOTIVAÇÃO:
-		 * Suponha um negócio de banco que possui uma conta comum e uma conta para
-		 * empresas, sendo que a conta para a empresa possui todos os membros da conta comum,
-		 * mais um limite de empréstimo e uma operação de realizar emprestimo. */
-		
-		
+		Account acc = new Account(1001, "Alex", 500.00);
+		BusinessAccount bacc = new BusinessAccount(1002, "Maria", 0.0, 500.0);
 
+		//UPCASTING
+		
+		Account acc1 = bacc; 
+		Account acc2 = new BusinessAccount(1002, "Bob", 0.0, 0.0);
+		Account acc3 = new SavingsAccount(1004, "Ana", 0.0, 0.01); 
+		
+		
+		//DOWNCASTING
+		
+		BusinessAccount acc4 = (BusinessAccount)acc2; 
+		acc4.loan(100.0);
+		
+		//BusinessAccount acc5 = (BusinessAccount) acc3;
+		
+		if(acc3 instanceof BusinessAccount) {
+			BusinessAccount acc5 = (BusinessAccount) acc3;
+			acc5.loan(200.0);
+			System.out.println("Loan!");
+		}
+		
+		if(acc3 instanceof SavingsAccount) {
+			SavingsAccount acc5 = (SavingsAccount) acc3;
+			acc5.updateBalance();
+			System.out.println("Update!");
+		}
+		
+		
+		
+		
 	}
 
 }
